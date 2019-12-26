0e52842ce4f6a534caaa5e431d9580f03c39c29b
xujiajun <392035561@qq.com>
2019-12-26 10:37:27 +0800

    增加exception

---
+++ src/main/java/com/dabanjia/vayne/core/exception/ResponseException.java
@@ -0,0 +1,30 @@
+package com.dabanjia.vayne.core.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author xujiajun
+ * @since 2019/12/26
+ */
+@Setter
+@Getter
+@ToString
+public class ResponseException extends RuntimeException {
+
+	private static final long serialVersionUID = 6607462217623613389L;
+
+	private Integer code;
+
+	private String message;
+
+	public ResponseException() {
+		super();
+	}
+
+	public ResponseException(Integer code, String message) {
+		super(message);
+		this.code = code;
+	}
+}
